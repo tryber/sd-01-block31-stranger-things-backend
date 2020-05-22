eaeddeae093559402ecb73fd58c84f1b7068a9ba
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-05-22 11:14:51 -0300

    corrigindo erro de deploy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const upsideDown = process.env.DEMOGORGON;
-const hereIsTheUpsideDown = false;
+const hereIsTheUpsideDown = new Boolean(upsideDown) || false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
